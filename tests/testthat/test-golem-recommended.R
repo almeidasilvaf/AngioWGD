cfe1158ebc84fe80b8cf75a412f58af49e8b6908
almeidasilvaf <fabricio_almeidasilva@hotmail.com>
2024-09-13 16:06:02 +0200

    Added basic structure for unit tests

---
+++ tests/testthat/test-golem-recommended.R
@@ -0,0 +1,17 @@
+
+test_that("app server", {
+  server <- app_server
+  expect_equal(class(server), "function")
+  # Check that formals have not been removed
+  fmls <- formals(app_server)
+  for (i in c("input", "output", "session")){
+    expect_true(i %in% names(fmls))
+  }
+})
+
+# Configure this test to fit your need
+test_that(
+  "app launches",{
+    golem::expect_running(sleep = 3)
+  }
+)
